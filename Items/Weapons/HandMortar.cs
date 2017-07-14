153e4cee2c22d30e4e83c3403efd612373388130
Thomas Volk <tvolk131@gmail.com>
2017-07-14 09:46:39 -0700

    Added random fire direction and muzzle compensation to Hand Mortar

--- Items/Weapons/HandMortar.cs
+++ Items/Weapons/HandMortar.cs
@@ -1,5 +1,11 @@
-using Terraria.ID;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
 using Terraria.ModLoader;
+using Terraria.ID;
 using Microsoft.Xna.Framework;
 
 namespace LovelyMod.Items.Weapons
@@ -45,65 +51,21 @@ namespace LovelyMod.Items.Weapons
 			return new Vector2(-3, 0);
 		}
 
-		// What if I wanted it to work like Uzi, replacing regular bullets with High Velocity Bullets?
-		// Uzi/Molten Fury style: Replace normal Bullets with Highvelocity
-		/*public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
 		{
-			if (type == ProjectileID.Bullet) // or ProjectileID.WoodenArrowFriendly
-			{
-				type = ProjectileID.BulletHighVelocity; // or ProjectileID.FireArrow;
-			}
-			return true; // return true to allow tmodloader to call Projectile.NewProjectile as normal
-		}*/
-
-		// What if I wanted an inaccurate gun? (Chain Gun)
-		// Inaccurate Gun style: Single Projectile, Random spread 
-		/*public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
-		{
-			Vector2 perturbedSpeed = new Vector2(speedX, speedY).RotatedByRandom(MathHelper.ToRadians(30));
-			speedX = perturbedSpeed.X;
-			speedY = perturbedSpeed.Y;
-			return true;
-		}*/
-
-		// What if I wanted multiple projectiles in a even spread? (Vampire Knives) 
-		// Even Arc style: Multiple Projectile, Even Spread 
-		/*public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
-		{
-			float numberProjectiles = 3 + Main.rand.Next(3); // 3, 4, or 5 shots
-			float rotation = MathHelper.ToRadians(45);
-			position += Vector2.Normalize(new Vector2(speedX, speedY)) * 45f;
-			for (int i = 0; i < numberProjectiles; i++)
-			{
-				Vector2 perturbedSpeed = new Vector2(speedX, speedY).RotatedBy(MathHelper.Lerp(-rotation, rotation, i / (numberProjectiles - 1))) * .2f; // Watch out for dividing by 0 if there is only 1 projectile.
-				Projectile.NewProjectile(position.X, position.Y, perturbedSpeed.X, perturbedSpeed.Y, type, damage, knockBack, player.whoAmI);
-			}
-			return false;
-		}*/
-
-		// How can I make the shots appear out of the muzzle exactly?
-		// Also, when I do this, how do I prevent shooting through tiles?
-		/*public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
-		{
+			//Makes the grenades appear just outside of the muzzle rather than inside it
 			Vector2 muzzleOffset = Vector2.Normalize(new Vector2(speedX, speedY)) * 25f;
 			if (Collision.CanHit(position, 0, 0, position + muzzleOffset, 0, 0))
 			{
 				position += muzzleOffset;
 			}
-			return true;
-		}*/
 
-		// How can I get a "Clockwork Assult Riffle" effect?
-		// 3 round burst, only consume 1 ammo for burst. Delay between bursts, use reuseDelay
-		/*	The following changes to SetDefaults()
-		 	item.useAnimation = 12;
-			item.useTime = 4;
-			item.reuseDelay = 14;
-		public override bool ConsumeAmmo(Player player)
-		{
-			// Because of how the game works, player.itemAnimation will be 11, 7, and finally 3. (UseAmination - 1, then - useTime until less than 0.) 
-			// We can get the Clockwork Assult Riffle Effect by not consuming ammo when itemAnimation is lower than the first shot.
-			return !(player.itemAnimation < item.useAnimation - 2);
-		}*/
+			//Makes the grenades shoot somewhat randomly
+			Vector2 perturbedSpeed = new Vector2(speedX, speedY).RotatedByRandom(MathHelper.ToRadians(10));
+			speedX = perturbedSpeed.X;
+			speedY = perturbedSpeed.Y;
+
+			return true;
+		}
 	}
 }
