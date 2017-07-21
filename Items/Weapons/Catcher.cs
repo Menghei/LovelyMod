90eca9185bc72a07909e4e18cb6aed64aea65e82
Thomas Volk <tvolk131@gmail.com>
2017-07-20 23:32:11 -0700

    Catcher created

---
+++ Items/Weapons/Catcher.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+
+namespace LovelyMod.Items.Weapons
+{
+	public class Catcher : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			Tooltip.SetDefault("Shoots grenades.");
+		}
+
+		public override void SetDefaults()
+		{
+			item.damage = 20;
+			item.ranged = true;
+			item.width = 40;
+			item.height = 20;
+			item.useTime = 20;
+			item.useAnimation = 20;
+			item.useStyle = 5;
+			item.noMelee = true; //so the item's animation doesn't do damage
+			item.knockBack = 4;
+			item.value = 10000;
+			item.rare = 2;
+			item.UseSound = SoundID.Item11;
+			item.autoReuse = true;
+			item.shoot = 10; //idk why but all the guns in the vanilla source have this
+			item.shootSpeed = 6f;
+			item.useAmmo = AmmoID.Bullet;
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.DirtBlock);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+
+		//Makes the gun held at the trigger rather than the stock
+		public override Vector2? HoldoutOffset()
+		{
+			return new Vector2(-6, 2);
+		}
+	}
+}
